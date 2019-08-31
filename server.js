c8cd16b9791baea6d98d0f5032ea4a4ac2cca057
Kyle Mulqueen <kmulqdrums710@gmail.com>
2019-08-30 21:33:07 -0600

    Register and login functionality.

--- server.js
+++ server.js
@@ -12,5 +12,7 @@ connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
